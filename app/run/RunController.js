0a9c5cd9cb6ea16269b4c821393a75c3b19558ff
l3s777 <lesly.zerna.bo07@gmail.com>
2016-12-14 15:18:21 +0100

    changes to graphs

--- app/run/RunController.js
+++ app/run/RunController.js
@@ -10,7 +10,7 @@ app.controller('RunController', ['$rootScope', '$scope', '$mdDialog', function($
   var fs = require('fs');
 
   // parallel coordinates
-  // var d3 = require("d3");
+  var d3 = require('d3');
   // var jsdom = require("jsdom");
   //
   // var document = jsdom.jsdom(),
@@ -133,18 +133,61 @@ app.controller('RunController', ['$rootScope', '$scope', '$mdDialog', function($
   }
 
   // parallel coordinates
-  var data = [
-    [0,-0,0,0,0,3 ],
-    [1,-1,1,2,1,6 ],
-    [2,-2,4,4,0.5,2],
-    [3,-3,9,6,0.33,4],
-    [4,-4,16,8,0.25,9]
-  ];
-
-  var pc = d3.parcoords()("#example")
-    .data(data)
-    .render()
-    .createAxes();
+  // const {ipcRenderer} = require("electron");
+  // var color = d3.scaleOrdinal(d3.schemeCategory20);
+  // var svg = d3.select("#svg");
+  // var svg_height = svg.attr("height");
+  // var svg_width = svg.attr("width");
+  // graph = ipcRenderer.sendSync("actionGetData", "JSON");
+  // var simulation = d3.forceSimulation()
+  //     .force("link", d3.forceLink().id(function(d) { return d.name; }))
+  //     .force("charge", d3.forceManyBody())
+  //     .force("center", d3.forceCenter(svg_width / 2, svg_height / 2));
+  // var link = svg.append("g")
+  //     .attr("class", "links")
+  //     .selectAll("line")
+  //     .data(graph.connections)
+  //     .enter().append("line")
+  //     .attr("stroke-width", function(d) { return 1; });
+  // var links = svg.append("g")
+  //     .attr("class", "links")
+  //     .selectAll("line")
+  //     .data(graph.connections)
+  //     .enter().append("line");
+  // var node = svg.append("g")
+  //     .attr("class", "nodes")
+  //     .selectAll("circle")
+  //     .data(graph.ifs)
+  //     .enter().append("circle")
+  //     .attr("r", 5)
+  //     .attr("fill", function(d) {
+  //         var vlan = 0;
+  //         if (d.vlan !== null)
+  //         {
+  //             for (var i = 1; i - 1 < graph.vlans.length; i += 1)
+  //             {
+  //                 if (graph.vlans[i - 1].name === d.vlan)
+  //                 {
+  //                     vlan = i;
+  //                     break;
+  //                 }
+  //             }
+  //         }
+  //         return color(vlan);
+  //     });
+  // node.append("title").text(function(d) { return d.name; });
+  // simulation.nodes(graph.ifs).on("tick", ticked);
+  // simulation.force("link").links(graph.connections);
+  // function ticked() {
+  //     link
+  //         .attr("x1", function(d) { return d.source.x; })
+  //         .attr("y1", function(d) { return d.source.y; })
+  //         .attr("x2", function(d) { return d.target.x; })
+  //         .attr("y2", function(d) { return d.target.y; });
+  //     node
+  //         .attr("cx", function(d) { return d.x; })
+  //         .attr("cy", function(d) { return d.y; });
+  // }
 
 
 }]);
