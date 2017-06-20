5c8873ba41e6b881f7d19493468a6d6b53e4f800
l3s777 <lesly.zerna.bo07@gmail.com>
2017-06-20 12:16:22 +0200

    minor changes at home screen

--- app/home/HomeController.js
+++ app/home/HomeController.js
@@ -1,1 +1,34 @@
-app.controller('HomeController', ['$scope', '$mdDialog', function($scope, $mdDialog) { }]);
+app.controller('HomeController', ['$rootScope', '$scope', '$mdDialog', function($rootScope, $scope, $mdDialog) {
+
+  // Dependencies
+  var app2 = require('electron').remote;
+  var dialog = app2.dialog;
+  var fs = require('fs');
+
+  var userPathWorkspace, userPathIRACE;
+
+  $scope.pathForWorkspace = function() {
+    userPathWorkspace = dialog.showOpenDialog({
+        properties: ['openDirectory']
+    });
+  }
+
+  $scope.pathForIrace = function() {
+    userPathIRACE = dialog.showOpenDialog({
+        properties: ['openDirectory']
+    });
+  }
+
+  $scope.validate = function() {
+    if(userPathWorkspace) {
+      if(userPathIRACE) {
+        $rootScope.pathWorkspace = userPathWorkspace;
+        $rootScope.userPathIRACE = userPathIRACE;
+
+        // $window.open('#setup');
+      }
+    } else {
+      dialog.showErrorBox("Error","Set the workspace and IRACE installation paths, please.");
+    }
+  }
+}]);
